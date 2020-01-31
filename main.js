b9bfb3dc93c89f41e0a407e284bacde6bf28f5c3
simon van de voort <zeemon777@gmail.com>
2020-01-31 19:13:37 +0100

    ist
    q

---
+++ main.js
@@ -0,0 +1,82 @@
+// gsap.from('#uberbar', {opacity: 0, duration: 1, y: -50});
+// gsap.from('.landing--img', {duration: 3, backgroundPosition: '0px -200px', opacity:0})
+// gsap.from('#videobox1', {duration: 2, x:150, opacity:0})
+
+var tl = gsap.timeline({defaults:{duration: 1}})
+var sl = gsap.timeline({paused: true})
+var text = gsap.timeline({paused: true})
+// var logo = gsap.timeline({paused:true})
+
+
+tl.from('.landing--img', {duration: 3, stagger: .2, backgroundPosition: '0px -200px', opacity:0});
+tl.from('#uberbar', {opacity: 0,  y: -50}, "-=2.8");
+tl.from('.landing--img', {height: '1vh'}, "-=2.8");
+tl.from('#zlogo', {duration: 3, opacity: 0, x: -350, y: -350, fontSize: '0px', ease: "power4.out"}, "=-1.8");
+
+// logo.to('#zlogo', {duration: 1, opacity: 0, x: -350, y: -350, ease: "power4.out"});
+
+sl.from('#videobox1', {duration: 1.5, ease: "back.out(1.7)", x:150, opacity:0,});
+
+text.from(".heading--table", {rotation: -37, ease: "back.out(0.7)", y: -100, opacity:0, duration: 1});
+
+
+document.querySelector('.divider').addEventListener('mouseover', letsAnimate);
+function letsAnimate() {
+    sl.play();
+    text.play();
+    
+    }
+
+// document.querySelector('.dividerone').addEventListener('mousover', letsAnimateLogo);
+// function letsAnimateLogo() {
+
+//     logo.play();
+// }
+
+// scroll animation
+
+// var tlscroll = new TimelineMax({onUpdate:updatePercentage});
+var tlscroll = gsap.timeline({onUpdate:updatePercentage})
+const controller = new ScrollMagic.Controller(); 
+
+// second timeline scroll
+var tlscroll2 = gsap.timeline({defaults:{duration: 5}})
+
+tlscroll.from("blockquote", 1,{x:200, opacity: 0});
+tlscroll.from("span", 1,{width: 0}, "=-.5");
+tlscroll.from("#alley", 3, {x:100,  ease: "back.out(1.7)", opacity: 0});
+tlscroll.from("#hallway", 1,{x:200,  ease: "back.out(1.7)", width: "1%", opacity: 0});
+
+
+tlscroll2.from("#box", 1, {opacity: 0, scale: 0});
+tlscroll2.to("#box", 3, {
+    left: "20%",
+    scale: 2,
+    borderColor: 'dark gray',
+    backgroundColor: 'gray',
+    
+    borderWidth: 1,
+    boxShadow: '1px 2px 0px rgba(0,0,0, .09)',
+    zIndex: '-1'
+    });
+
+const scene = new  ScrollMagic.Scene({
+    triggerElement: ".sticky",
+    triggerHook: "onLeave",
+    duration: "100%"
+})
+   .setPin(".sticky")
+   .setTween(tlscroll)
+   .addTo(controller);
+
+function updatePercentage() {
+    tl.progress();
+    console.log(tl.progress());
+}
+
+// 2nd scrollmagic scene
+const scene2 = new  ScrollMagic.Scene({
+    triggerElement: "blockquote"
+})
+    .setTween(tlscroll2)
+    .addTo(controller);
